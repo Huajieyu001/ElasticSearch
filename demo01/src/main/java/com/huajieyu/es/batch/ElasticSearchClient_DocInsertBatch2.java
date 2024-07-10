d1841ed55b83cba1e9e474e20d1fd3685a1bd86e
Huajieyu001 <331173500@qq.com>
2024-07-11 00:08:34 +0800

    补充commit

--- demo01/src/main/java/com/huajieyu/es/batch/ElasticSearchClient_DocInsertBatch2.java
+++ demo01/src/main/java/com/huajieyu/es/batch/ElasticSearchClient_DocInsertBatch2.java
@@ -1,18 +1,15 @@
 package com.huajieyu.es.batch;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.huajieyu.es.User;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 
-public class ElasticSearchClient_DocInsertBatch {
+public class ElasticSearchClient_DocInsertBatch2 {
 
     public static void main(String[] args) throws Exception{
 
@@ -21,10 +18,13 @@ public class ElasticSearchClient_DocInsertBatch {
 
         // 批量插入数据，如果不存在则为插入，如果已存在则为更新
         BulkRequest request = new BulkRequest();
-        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "1111"));
-        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "2222"));
-        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "wangwu"));
-        request.add(new IndexRequest().index("user").id("1004").source(XContentType.JSON, "name", "zhaoliu"));
+        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "asdfg", "age", 45, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "asdfg", "age", 32, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "asdfg", "age", 42, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1004").source(XContentType.JSON, "name", "asdfg", "age", 21, "sex", "男"));
+        request.add(new IndexRequest().index("user").id("1005").source(XContentType.JSON, "name", "asdfg", "age", 54, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1006").source(XContentType.JSON, "name", "asdfg", "age", 24, "sex", "女"));
+        request.add(new IndexRequest().index("user").id("1007").source(XContentType.JSON, "name", "asdfg", "age", 45, "sex", "男"));
 
         BulkResponse bulk = highLevelClient.bulk(request, RequestOptions.DEFAULT);
 
--- demo01/src/main/java/com/huajieyu/es/batch/ElasticSearchClient_DocQuery.java
+++ demo01/src/main/java/com/huajieyu/es/batch/ElasticSearchClient_DocQuery.java
@@ -4,31 +4,97 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 
-public class ElasticSearchClient_DocInsertBatch2 {
+public class ElasticSearchClient_DocQuery {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         RestHighLevelClient highLevelClient =
                 new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
 
-        // 批量插入数据，如果不存在则为插入，如果已存在则为更新
-        BulkRequest request = new BulkRequest();
-        request.add(new IndexRequest().index("user").id("1001").source(XContentType.JSON, "name", "asdfg", "age", 45, "sex", "男"));
-        request.add(new IndexRequest().index("user").id("1002").source(XContentType.JSON, "name", "asdfg", "age", 32, "sex", "男"));
-        request.add(new IndexRequest().index("user").id("1003").source(XContentType.JSON, "name", "asdfg", "age", 42, "sex", "女"));
-        request.add(new IndexRequest().index("user").id("1004").source(XContentType.JSON, "name", "asdfg", "age", 21, "sex", "男"));
-        request.add(new IndexRequest().index("user").id("1005").source(XContentType.JSON, "name", "asdfg", "age", 54, "sex", "女"));
-        request.add(new IndexRequest().index("user").id("1006").source(XContentType.JSON, "name", "asdfg", "age", 24, "sex", "女"));
-        request.add(new IndexRequest().index("user").id("1007").source(XContentType.JSON, "name", "asdfg", "age", 45, "sex", "男"));
+        SearchRequest request = new SearchRequest();
 
-        BulkResponse bulk = highLevelClient.bulk(request, RequestOptions.DEFAULT);
+        // 指定索引
+        request.indices("user");
 
-        System.out.println(bulk);
+        // 1.无条件则查询user索引下的所有数据
+        SearchResponse response = highLevelClient.search(request, RequestOptions.DEFAULT);
+
+        System.out.println(response);
+        // 查看访问时间
+        System.out.println(response.getTook());
+
+        System.out.println("-----------------------------------------------");
+
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+
+        // 无条件则查询user索引下的所有数据
+        request.indices("user").source(builder);
+        response = highLevelClient.search(request, RequestOptions.DEFAULT);
+        System.out.println(response);
+        System.out.println(response.getTook());
+
+        System.out.println("foreach start:");
+        response.getHits().forEach(System.out::println);
+
+        // 2.条件查询
+        System.out.println("------------------------------2.条件查询");
+        TermQueryBuilder ageQuery = QueryBuilders.termQuery("sex", "男");
+
+        request.indices("user").source(new SearchSourceBuilder().query(ageQuery));
+        response = highLevelClient.search(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getTook());
+        response.getHits().forEach(System.out::println);
+
+        // 3.分页查询
+        System.out.println("------------------------------3.分页查询");
+
+        SearchSourceBuilder builder1 = new SearchSourceBuilder();
+        builder1.query(QueryBuilders.matchAllQuery());
+        // 从第2条开始查询（条数下标是从0开始的，类似数组）
+        builder1.from(2);
+        // 指定查询的件数为3
+        builder1.size(3);
+        request.indices("user").source(builder1);
+        response = highLevelClient.search(request, RequestOptions.DEFAULT);
+        response.getHits().forEach(System.out::println);
+
+        // 4.查询排序
+        System.out.println("------------------------------4.查询排序");
+        SearchSourceBuilder builder2 = new SearchSourceBuilder();
+        // 按age字段升序排列，升序可省略，默认为升序ASC
+        builder2.sort("age", SortOrder.ASC);
+        request.indices("user").source(builder2);
+
+        response = highLevelClient.search(request, RequestOptions.DEFAULT);
+        System.out.println(response.getTook());
+        response.getHits().forEach(System.out::println);
+
+        // 5.过滤字段
+        System.out.println("------------------------------4.查询排序");
+        SearchSourceBuilder builder3 = new SearchSourceBuilder();
+
+        // 保留name，排除age字段。sex字段没写，但是因为includes只包含name，所以sex默认排除，
+        // 如果includes长度为0，则排除的字段之外的所有字段都会显示
+        String [] includes = new String[]{};
+        String [] excludes = new String[]{"age"};
+        builder3.fetchSource(includes, excludes);
+        request.indices("user").source(builder3);
+        response = highLevelClient.search(request, RequestOptions.DEFAULT);
+
+        response.getHits().forEach(System.out::println);
 
         highLevelClient.close();
     }
